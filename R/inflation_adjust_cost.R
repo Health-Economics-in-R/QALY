909536a9e8e0a950d7ec08e1ec68486453e85933
Nathan Green <n8thangreen@yahoo.co.uk>
2017-02-20 17:01:46 +0000

    extend function
    * add argument so can have fixed 3.5% inflation factor as well as the specific annual rate from GDP deflators table

--- R/inflation_adjust_cost.R
+++ R/inflation_adjust_cost.R
@@ -1,14 +1,18 @@
 
 #' Calculate Inflation Adjusted Costs
 #'
-#' Use ONS GDP_Deflators_Qtrly_National_Accounts
+#' up to the present time inflated upwards.
+#' Option to use the ONS GDP_Deflators_Qtrly_National_Accounts or
+#' a fixed 3.5\%.
 #' Can't download directly into function because the csv on the website is too messy as-is.
 #' This would be good to do though so that can always use latest version.
+#' TODO: webscraping? regular expressions?
 #'
-#' @param from_year Date of cost to convert
+#' @param from_year Date of cost to convert from
 #' @param to_year Date to convert cost to
 #' @param from_cost Cost at \code{from_year}
-#' @param reference Source of data
+#' @param reference Source of data (string)
+#' @param fixed Fixed 3.5\% rate of inflation?
 #'
 #' @return
 #' @export
@@ -18,10 +22,18 @@
 #' to_year <- 2015
 #' from_cost <- 96.140
 #'
-#' inflation_adjust_cost(from_year, to_year, from_cost)
+#' inflation_adjust_cost(from_year, to_year, from_cost, fixed = FALSE)
 #' #100
 #'
-inflation_adjust_cost <- function(from_year, to_year, from_cost, reference = NA){
+#' inflation_adjust_cost(from_year=2010, to_year=2016, from_cost=1)
+#' #1.229255
+#' 1*(1+0.035)^6
+#'
+inflation_adjust_cost <- function(from_year,
+                                  to_year,
+                                  from_cost,
+                                  reference = NA,
+                                  fixed = TRUE){
 
   ##TODO##
   # the webpage defaltor file is too messy- with comments etc- to use as raw data
@@ -42,7 +54,11 @@ inflation_adjust_cost <- function(from_year, to_year, from_cost, reference = NA)
   from_row <- which(deflators$`Calendar year`==from_year)
   to_row <- which(deflators$`Calendar year`==to_year)
 
-  deflators$prop_change_previous_year <- deflators$`per cent change on previous year`/100
+  if (fixed){
+    deflators$prop_change_previous_year <- 0.035
+  }else{
+    deflators$prop_change_previous_year <- deflators$`per cent change on previous year`/100
+  }
 
   to_cost <- from_cost
 
